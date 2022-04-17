cadf56291f52c26be580dc16c32ec4d687481026
fara0000 <fi@livereach.com>
2022-04-17 14:37:07 +0300

    added

--- src/components/sessions/SessionDescription.tsx
+++ src/components/sessions/SessionDescription.tsx
@@ -3,6 +3,9 @@ import { Box, Text, Button, Flex, Grid } from "@chakra-ui/react";
 import {botSession, sessionsType} from "../../types";
 import sessionStore from "../../store/session";
 import {useHistory} from "react-router-dom";
+import CellTowerIcon from '@mui/icons-material/CellTower';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+
 
 interface Props {
     id: number;
@@ -19,9 +22,7 @@ interface Props {
     setSelected: any;
 }
 
-
-
-export const SessionDescription : FC<Props> = ({ item, setSelected, id, status, sessionName, customerName, location, start, end, bet }) => {
+export const SessionDescription : FC<Props> = ({ item, id, status, sessionName, customerName, location, start, end, bet }) => {
     const history = useHistory();
 
     return (
@@ -31,16 +32,27 @@ export const SessionDescription : FC<Props> = ({ item, setSelected, id, status, 
             h='142px'
             bg={'#050407'}
             color={"#FFFFFF"}
-            mt='42px'
+            border='3px'
+            borderColor='grey'
+            mt='32px'
+            mb='15px'
             borderRadius="6px"
+            cursor="pointer"
             onClick={() => {
                 sessionStore.getSessionId(id).then(res => res)
                 history.push('/sessions/' + id)
             }}
         >
             <Box w='392px' ml='23px'>
-                <Text fontSize='20px'>{sessionName}</Text>
-                <Text fontSize='14px' mt='2px'>{customerName}</Text>
+                <Text mt='20px' fontSize='20px'>{sessionName}</Text>
+                <Text
+                    mb='5px' color='gray.500' fontSize='14px' mt='2px'
+                    _hover={{
+                        color: '#3047FE',
+                    }}
+                >
+                    {customerName}
+                </Text>
                 <Flex
                     justify="center"
                 >
@@ -60,17 +72,17 @@ export const SessionDescription : FC<Props> = ({ item, setSelected, id, status, 
                         border='1px'
                         borderColor='#3047FE'
                         bg={'#050407'}
-                        h='24px'
+                        h='28px'
                         w='108px'
-                        mt='16px'
+                        mt='26px'
                     >
                         <Flex
                             justify="center"
                         >
-                            <Box>
-
+                            <Box color='#3047FE'>
+                                <CellTowerIcon fontSize='small'/>
                             </Box>
-                            <Text w='65px' h='17px' fontSize='14px'>{status}</Text>
+                            <Text mt='3px' ml='8px' w='65px' h='17px' fontSize='14px'>{status}</Text>
                         </Flex>
                     </Box>
                 </Flex>
@@ -78,12 +90,12 @@ export const SessionDescription : FC<Props> = ({ item, setSelected, id, status, 
                     justify='right'
                     mt='46px'
                 >
-                    <Box>
-
+                    <Box mb='5px' color='#3047FE'>
+                        <FmdGoodIcon fontSize='small'/>
                     </Box>
-                    <Text align='right' w='148px' h='17px' fontSize='14px'>{location}</Text>
+                    <Text mb='10px' align='center' w='148px' h='17px' fontSize='14px'>{location}</Text>
                 </Flex>
             </Box>
         </Flex>
-    )
+    );
 }
