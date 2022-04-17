ae0e93462f1e29a381983e00d651f4f8775f8b63
fara0000 <fi@livereach.com>
2022-04-17 05:10:12 +0300

    added

--- src/views/profile/components/autobets/components/AutoBetsItem.tsx
+++ src/views/profile/components/autobets/components/AutoBetsItem.tsx
@@ -1,23 +1,20 @@
 import {Flex, Text} from "@chakra-ui/react";
-import React from "react";
+import React, {FC} from "react";
 import {AllBets, PriorityBets} from "../AutoBets";
+import {autoSessionsType} from "../../../../../types";
 
-interface AutoBetsItemProps {
-
-};
-
-export const AutoBetsItem = () => {
+export const AutoBetsItem: FC<autoSessionsType> = ({ sessionName, currentPrice, customerName }) => {
     return (
-        <Flex border="1px solid #FFFFFF" w="548px" h="101px" ml="18px" borderRadius="6px" cursor="pointer">
+        <Flex border="1px solid #FFFFFF" w="548px" h="101px" borderRadius="6px" cursor="pointer" mt="9px">
             <Flex p="13px 0 9px 19px" h="100%" w="60%" flexDir="column">
-                <Text fontSize='16px'>Сессия №1</Text>
-                <Text fontSize='10px' mt='2px'>Описание учреждения, объявившего сессию</Text>
+                <Text fontSize='16px'>{sessionName}</Text>
+                <Text fontSize='10px' mt='2px'>{customerName}</Text>
             </Flex>
             <Flex p="5px 7px 9px 0" h="100%" w="40%" alignItems="end" flexDir="column">
                 <Flex flexDir="column" alignItems="end">
                     <Text fontSize='12px'>Текущая цена</Text>
                     <Flex fontSize="12px" w="103px" h="19px" border={'1px solid #3047FE'} justifyContent="center" alignItems="center" borderRadius="4px">
-                        15.000 rub
+                        {currentPrice}
                     </Flex>
                 </Flex>
                 <Flex flexDir="column" alignItems="end" mt="14px">
